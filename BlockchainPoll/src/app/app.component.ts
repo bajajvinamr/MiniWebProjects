7f24ce4cff0ec6cf563cf1e49fb20cf347c617f7
bajajvinamr <lit2019023@iiitl.ac.in>
2021-04-26 11:10:16 +0530

    Updated poll

--- BlockchainPoll/src/app/app.component.ts
+++ BlockchainPoll/src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 showForm = false;
+
+
+  poll =
+  {
+  question: "Which is your favorite sport",
+  image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/92/Youth-soccer-indiana.jpg/1200px-Youth-soccer-indiana.jpg',
+  votes : [0,5,7,1],
+  voted: true,
+  };
 }
